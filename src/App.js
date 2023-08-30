a0c652e54eb9c9fe5d44cf6c81e574cc2b42269d
arun <arun.chan86@gmail.com>
2023-08-30 14:52:03 +0530

    02-Create-Store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-
 function App() {
   return (
-   <>
-   <h1 class="text-3xl font-bold text-pink-500">Redux Store</h1>
-   
-   </>
+    <>
+      <h1 class="text-center text-3xl font-bold mt-2 py-4 text-pink-500">
+        Redux Store
+      </h1>
+    </>
   );
 }
 
